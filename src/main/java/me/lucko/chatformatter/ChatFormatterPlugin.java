0a36a91167a5eb9f9a527ea41c933fe57b214ec4
Luck <git@lucko.me>
2017-10-30 22:11:42 +0000

    Initial commit

---
+++ src/main/java/me/lucko/chatformatter/ChatFormatterPlugin.java
@@ -0,0 +1,100 @@
+package me.lucko.chatformatter;
+
+import net.milkbowl.vault.chat.Chat;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.server.ServiceRegisterEvent;
+import org.bukkit.event.server.ServiceUnregisterEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * A super simple chat formatting plugin using Vault.
+ */
+public class ChatFormatterPlugin extends JavaPlugin implements Listener {
+
+    private static final String NAME_PLACEHOLDER = "{name}";
+    private static final String MESSAGE_PLACEHOLDER = "{message}";
+    private static final String PREFIX_PLACEHOLDER = "{prefix}";
+    private static final String SUFFIX_PLACEHOLDER = "{suffix}";
+    private static final String DEFAULT_FORMAT = "<" + PREFIX_PLACEHOLDER + NAME_PLACEHOLDER + SUFFIX_PLACEHOLDER + "> " + MESSAGE_PLACEHOLDER;
+
+    private String format;
+    private Chat vaultChat = null;
+
+    @Override
+    public void onEnable() {
+        saveDefaultConfig();
+        reloadConfigValues();
+        refreshVault();
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    private void reloadConfigValues() {
+        format = colorize(getConfig().getString("format", DEFAULT_FORMAT)
+                .replace(NAME_PLACEHOLDER, "%1$s")
+                .replace(MESSAGE_PLACEHOLDER, "%2$s"));
+    }
+
+    private void refreshVault() {
+        Chat vaultChat = getServer().getServicesManager().load(Chat.class);
+        if (vaultChat != this.vaultChat) {
+            getLogger().info("New Vault Chat implementation registered: " + (vaultChat == null ? "null" : vaultChat.getName()));
+        }
+        this.vaultChat = vaultChat;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (args.length != 0 && args[0].equalsIgnoreCase("reload")) {
+            reloadConfig();
+            reloadConfigValues();
+
+            sender.sendMessage("Reloaded successfully.");
+            return true;
+        }
+
+        return false;
+    }
+
+    @EventHandler
+    public void onServiceChange(ServiceRegisterEvent e) {
+        if (e.getProvider().getService() == Chat.class) {
+            refreshVault();
+        }
+    }
+
+    @EventHandler
+    public void onServiceChange(ServiceUnregisterEvent e) {
+        if (e.getProvider().getService() == Chat.class) {
+            refreshVault();
+        }
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onChatLow(AsyncPlayerChatEvent e) {
+        e.setFormat(format);
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onChatHigh(AsyncPlayerChatEvent e) {
+        String format = e.getFormat();
+        if (vaultChat != null && format.contains(PREFIX_PLACEHOLDER)) {
+            format = format.replace(PREFIX_PLACEHOLDER, colorize(vaultChat.getPlayerPrefix(e.getPlayer())));
+        }
+        if (vaultChat != null && format.contains(SUFFIX_PLACEHOLDER)) {
+            format = format.replace(SUFFIX_PLACEHOLDER, colorize(vaultChat.getPlayerSuffix(e.getPlayer())));
+        }
+        e.setFormat(format);
+    }
+
+    private static String colorize(String s) {
+        return s == null ? null : ChatColor.translateAlternateColorCodes('&', s);
+    }
+
+}
