7ab319734a4e0b3a6db2c327f7489f8c97493817
Luck <git@lucko.me>
2020-10-26 12:04:22 +0000

    Add support for the "nicer" legacy hex color format

--- src/main/java/me/lucko/chatformatter/ChatFormatterPlugin.java
+++ src/main/java/me/lucko/chatformatter/ChatFormatterPlugin.java
@@ -13,19 +13,40 @@ import org.bukkit.event.server.ServiceRegisterEvent;
 import org.bukkit.event.server.ServiceUnregisterEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.function.Supplier;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * A super simple chat formatting plugin using Vault.
  */
 public class ChatFormatterPlugin extends JavaPlugin implements Listener {
 
+    // Format placeholders
     private static final String NAME_PLACEHOLDER = "{name}";
     private static final String DISPLAYNAME_PLACEHOLDER = "{displayname}";
     private static final String MESSAGE_PLACEHOLDER = "{message}";
     private static final String PREFIX_PLACEHOLDER = "{prefix}";
     private static final String SUFFIX_PLACEHOLDER = "{suffix}";
+
+    // Format placeholder patterns
+    private static final Pattern NAME_PLACEHOLDER_PATTERN = Pattern.compile(NAME_PLACEHOLDER, Pattern.LITERAL);
+    private static final Pattern PREFIX_PLACEHOLDER_PATTERN = Pattern.compile(PREFIX_PLACEHOLDER, Pattern.LITERAL);
+    private static final Pattern SUFFIX_PLACEHOLDER_PATTERN = Pattern.compile(SUFFIX_PLACEHOLDER, Pattern.LITERAL);
+
+    /** The default format */
     private static final String DEFAULT_FORMAT = "<" + PREFIX_PLACEHOLDER + NAME_PLACEHOLDER + SUFFIX_PLACEHOLDER + "> " + MESSAGE_PLACEHOLDER;
 
+    /** Pattern matching "nicer" legacy hex chat color codes - &#rrggbb */
+    private static final Pattern NICER_HEX_COLOR_PATTERN = Pattern.compile("&#([0-9a-fA-F]{6})");
+
+    /** The format used by this chat formatter instance */
     private String format;
+
+    /**
+     * The current Vault chat implementation registered on the server.
+     * Automatically updated as new services are registered.
+     */
     private Chat vaultChat = null;
 
     @Override
@@ -37,7 +58,9 @@ public class ChatFormatterPlugin extends JavaPlugin implements Listener {
     }
 
     private void reloadConfigValues() {
-        format = colorize(getConfig().getString("format", DEFAULT_FORMAT)
+        // Replace the {displayname} and {message} placeholders used in our format with
+        // the String.format tokens used by Bukkit.
+        this.format = colorize(getConfig().getString("format", DEFAULT_FORMAT)
                 .replace(DISPLAYNAME_PLACEHOLDER, "%1$s")
                 .replace(MESSAGE_PLACEHOLDER, "%2$s"));
     }
@@ -79,24 +102,67 @@ public class ChatFormatterPlugin extends JavaPlugin implements Listener {
 
     @EventHandler(priority = EventPriority.LOWEST)
     public void onChatLow(AsyncPlayerChatEvent e) {
-        e.setFormat(format);
+        // Set out format on the lowest priority - allow other plugins to override or add their own parts.
+        e.setFormat(this.format);
     }
 
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onChatHigh(AsyncPlayerChatEvent e) {
+        // Replace our placeholders on highest - just before
         String format = e.getFormat();
-        if (vaultChat != null && format.contains(PREFIX_PLACEHOLDER)) {
-            format = format.replace(PREFIX_PLACEHOLDER, colorize(vaultChat.getPlayerPrefix(e.getPlayer())));
+
+        if (this.vaultChat != null) {
+            format = replaceAll(PREFIX_PLACEHOLDER_PATTERN, format, () -> colorize(this.vaultChat.getPlayerPrefix(e.getPlayer())));
+            format = replaceAll(SUFFIX_PLACEHOLDER_PATTERN, format, () -> colorize(this.vaultChat.getPlayerSuffix(e.getPlayer())));
         }
-        if (vaultChat != null && format.contains(SUFFIX_PLACEHOLDER)) {
-            format = format.replace(SUFFIX_PLACEHOLDER, colorize(vaultChat.getPlayerSuffix(e.getPlayer())));
-        }
-        format = format.replace(NAME_PLACEHOLDER, e.getPlayer().getName());
+        format = replaceAll(NAME_PLACEHOLDER_PATTERN, format, () -> e.getPlayer().getName());
+
         e.setFormat(format);
     }
 
-    private static String colorize(String s) {
-        return s == null ? null : ChatColor.translateAlternateColorCodes('&', s);
+    /**
+     * Equivalent to {@link String#replace(CharSequence, CharSequence)}, but uses a
+     * {@link Supplier} for the replacement.
+     *
+     * @param pattern the pattern for the replacement target
+     * @param input the input string
+     * @param replacement the replacement
+     * @return the input string with the replacements applied
+     */
+    private static String replaceAll(Pattern pattern, String input, Supplier<String> replacement) {
+        Matcher matcher = pattern.matcher(input);
+        if (matcher.find()) {
+            return matcher.replaceAll(Matcher.quoteReplacement(replacement.get()));
+        }
+        return input;
+    }
+
+    /**
+     * Translates color codes in the given input string.
+     *
+     * @param string the string to "colorize"
+     * @return the colorized string
+     */
+    private static String colorize(String string) {
+        if (string == null) {
+            return "null";
+        }
+
+        // Convert from the '&#rrggbb' hex color format to the '&x&r&r&g&g&b&b' one used by Bukkit.
+        Matcher matcher = NICER_HEX_COLOR_PATTERN.matcher(string);
+        StringBuffer sb = new StringBuffer();
+
+        while (matcher.find()) {
+            StringBuilder replacement = new StringBuilder(14).append("&x");
+            for (char character : matcher.group(1).toCharArray()) {
+                replacement.append('&').append(character);
+            }
+            matcher.appendReplacement(sb, replacement.toString());
+        }
+        matcher.appendTail(sb);
+
+        // Translate from '&' to '§' (section symbol)
+        return ChatColor.translateAlternateColorCodes('&', sb.toString());
     }
 
 }
